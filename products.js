3de3d806566cbea0c0b5519eb9cd2f39a2171578
mujahidulislammd457-lgtm <mujahidulislammd457@gmail.com>
2026-01-15 22:57:01 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,5 @@
+const products = [
+  {id:1,name:"Dell Inspiron",price:45000,category:"Laptops",image:"https://via.placeholder.com/300"},
+  {id:2,name:"HP Pavilion",price:52000,category:"Laptops",image:"https://via.placeholder.com/300"},
+  {id:3,name:"Gaming Mouse",price:1200,category:"Accessories",image:"https://via.placeholder.com/300"}
+];
